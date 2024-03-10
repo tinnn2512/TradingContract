93ae3e318c944c34b78d8d938272be8280108c0e
Nguyễn Tín <nguyenntin2512@gmail.com>
2024-03-10 11:41:58 +0700

    first commit

---
+++ contracts/AccessManager.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AccessManager {
+    // Mapping để lưu trữ vai trò của từng địa chỉ
+    mapping(address => bool) public isAdmin;
+    mapping(address => bool) public isNotary;
+    mapping(address => bool) public isUser;
+
+    // Sự kiện được kích hoạt khi một vai trò mới được cấp
+    event RoleGranted(address indexed account, string role);
+
+    // Sự kiện được kích hoạt khi một vai trò bị thu hồi
+    event RoleRevoked(address indexed account, string role);
+
+    // Hàm khởi tạo, chỉ được gọi một lần khi triển khai hợp đồng
+    constructor() {
+        // Gán vai trò ADMIN_ROLE cho người triển khai hợp đồng
+        isAdmin[msg.sender] = true;
+        emit RoleGranted(msg.sender, "ADMIN");
+    }
+
+    // Hàm để cấp quyền cho một tài khoản
+    function grantNotaryRole(address account) external {
+        // Kiểm tra xem người gọi hàm có quyền ADMIN_ROLE không
+        require(isAdmin[msg.sender], "Only admins can grant roles");
+
+        // Cấp quyền NOTARY_ROLE cho tài khoản được chỉ định
+        isNotary[account] = true;
+        isUser[account] = false;
+
+        emit RoleGranted(account, "NOTARY");
+    }
+
+    // Hàm để thu hồi quyền NOTARY_ROLE của một tài khoản
+    function revokeNotaryRole(address account) external {
+        // Kiểm tra xem người gọi hàm có quyền ADMIN_ROLE không
+        require(isAdmin[msg.sender], "Only admins can revoke roles");
+
+        // Thu hồi quyền NOTARY_ROLE của tài khoản được chỉ định
+        isNotary[account] = false;
+        emit RoleRevoked(account, "NOTARY");
+    }
+
+    // Hàm để kiểm tra xem một tài khoản có vai trò NOTARY_ROLE hay không
+    function hasNotaryRole(address account) external view returns (bool) {
+        return isNotary[account];
+    }
+}
---
+++ contracts/LandLicenseRegistry.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+// // Uncomment this line to use console.log
+// // // import "hardhat/console.sol";
+// // import "./RoleManager.sol";
+// import "./RoleManager.sol";
+import "./AccessManager.sol";
+
+
+contract LandLicenseRegistry  {
+    struct LandLicense {
+        address owner;
+        string landAddress;
+        uint256 area;
+        string ipfsHash;
+    }
+//     RoleManager public roleManager;
+
+    mapping(uint256 => LandLicense) public landLicenses;
+    mapping(address => uint256[]) public userLandLicenses;
+
+//     modifier onlyNotary {
+//         // call contract roleContract để kiểm tra quyền
+//         require(roleManager.hasNotaryRole(msg.sender), "Notary permission required");
+//         _;
+//     }
+
+AccessManager public accessManager;
+
+
+modifier onlyNotary {
+    require(accessManager.hasNotaryRole(msg.sender), "Notary permission required");
+    _;
+}
+
+    event LandLicenseRegistered(uint256 licenseId, address indexed owner, string landAddress, uint256 area);
+
+//     constructor(RoleManager _roleManager){
+//         roleManager = _roleManager;
+//     }
+
+constructor(AccessManager _accessManager) {
+        accessManager = _accessManager;
+    }
+    
+    function registerLandLicense(address _owner, uint256 _licenseId, string memory _landAddress, uint256 _area, string memory _ipfsHash) external onlyNotary {
+    require(_owner != address(0), "Invalid owner address");
+    require(bytes(_landAddress).length > 0, "Land address must be provided");
+    require(_area > 0, "Area must be greater than 0");
+    require(bytes(_ipfsHash).length > 0, "ipfsHash must be provided");
+    // kiểm tra landLicenses[licenseId] là chưa có
+
+        // uint256 licenseId = landLicenses.length;
+        LandLicense storage newLicense = landLicenses[_licenseId];
+        newLicense.owner = _owner;
+        newLicense.landAddress = _landAddress;
+        newLicense.area = _area;
+        newLicense.ipfsHash = _ipfsHash;
+
+        userLandLicenses[_owner].push(_licenseId);
+
+        emit LandLicenseRegistered(_licenseId, msg.sender, _landAddress, _area);
+    }
+
+    function getUserLandLicenses() external view returns (uint256[] memory) {
+        return userLandLicenses[msg.sender];
+    }
+
+    function getLandLicense(uint256 _licenseId) external view returns (address owner, string memory landAddress, uint256 area, string memory ipfsHash) {
+        LandLicense storage landLicense = landLicenses[_licenseId];
+        require(landLicense.owner != address(0), "Land license not found");
+
+        owner = landLicense.owner;
+        landAddress = landLicense.landAddress;
+        area = landLicense.area;
+        ipfsHash = landLicense.ipfsHash;
+    }
+}
+
+
